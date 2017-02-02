3412cc8aca780726a86d58655bc57c5ad7c1066a
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-02 08:22:57 +0700

    update controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,30 @@
 class WorkoutsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to @workout
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
